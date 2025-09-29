3455d62ec0a0d447732d87498ad7d873156f48b5
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-09-29 12:44:31 +0200

    Añado saltos de línea

--- subir_a_github.sh
+++ subir_a_github.sh
@@ -2,7 +2,7 @@
 set -e
 if [ -z "$*" ]; then
   COMMIT_MSG="Añado ficheros"
-  echo "\nNo se proporcionó mensaje de commit. Usando mensaje por defecto: '$COMMIT_MSG'\n"
+  echo -e "\nNo se proporcionó mensaje de commit. Usando mensaje por defecto: '$COMMIT_MSG'\n"
 else
   COMMIT_MSG="$*"
 fi
@@ -10,10 +10,10 @@ git status
 
 git add .
 
-git commit -m "$COMMIT_MSG" || echo "\nNo hay cambios para añadir.\n"
+git commit -m "$COMMIT_MSG" || echo -e "\nNo hay cambios para añadir.\n"
 
-git pull || { echo "\nError en el git pull.\n"; exit 1; }
+git pull || { echo -e "\nError en el git pull.\n"; exit 1; }
 
-git push || { echo "\nError en el git push.\n"; exit 2; }
+git push || { echo -e "\nError en el git push.\n"; exit 2; }
 
-echo "\nOperacion completada con éxito"
+echo -e "\nOperacion completada con éxito"
