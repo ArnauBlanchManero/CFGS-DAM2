9989dac6df89c85c7700aa1be6749b5ff02c1673
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-09-30 10:26:36 +0200

    Ejercicios para aprender a lanzar procesos

---
+++ Programacion_de_Servicios_y_Procesos/EjecutarProcesos/src/PracitcaLanzarProceso/Main.java
@@ -0,0 +1,15 @@
+	package PracitcaLanzarProceso;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LanzarProceso levantar = new LanzarProceso();
+		for (int i = 0; i < 1; i++) {
+			levantar.ejecutar();
+		}
+		System.out.println("Proceso ejecutado.");
+
+
+	}
+
+}
