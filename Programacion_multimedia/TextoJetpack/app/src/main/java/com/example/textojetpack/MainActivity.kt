2b07ec6febc80d7be02bef52de97a390d8979577
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-10-03 13:55:10 +0200

    Aprendemos a modificar aspectos de una imagen

--- Programacion_multimedia/TextoJetpack/app/src/main/java/com/example/textojetpack/MainActivity.kt
+++ Programacion_multimedia/TextoJetpack/app/src/main/java/com/example/textojetpack/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.textojetpack
 
-import android.media.Image
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -28,11 +28,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +48,8 @@ class MainActivity : ComponentActivity() {
             //FilasColumnas()
             //ColumnasInternas()
             //Cajas()
-            Imagen()
+            //Imagen()
+            ImagenZoom()
         }
     }
 }
@@ -111,13 +111,13 @@ fun Cajas(){
 }
 
 @Composable
-fun Imagen() {
+fun Imagen(painter: Painter) {
     var colorFondo by remember { mutableStateOf(Color.Blue) }
     var posicionTexto by remember { mutableStateOf(Offset(40f,0f)) }
     var posicionImg by remember { mutableStateOf(Offset(40f,0f)) }
     Box(modifier = Modifier.fillMaxSize().padding(top = 40.dp).background(colorFondo)) {
         Image(
-            painter = painterResource(id = R.drawable.turtle),
+            painter = painter,
             contentDescription = "Dibujo de una tortuga",
             modifier = Modifier.align(Alignment.Center).fillMaxSize().offset {
                 IntOffset(x = posicionImg.x.toInt(), y = posicionImg.y.toInt())
@@ -160,10 +160,49 @@ fun colorRandom(): Color{
     var azul = (0..255).random()
     return Color(red = rojo, green = verde,blue = azul)
 }
+@Composable
+fun ImagenZoom(){
+    var escalaImagen by remember { mutableStateOf(value = 1f) }// 1f escala real 1:1
+    var rotacionImagen by remember { mutableStateOf(value = 0f) }
+    var posicionImagen by remember { mutableStateOf(value = Offset(x=0f, y=0f)) }
+    Box(modifier = Modifier.fillMaxSize()
+        .pointerInput(key1 = Unit){
+            detectTransformGestures { _, desplazamiento, zoom, rotacion, ->
+                escalaImagen *= zoom
+                posicionImagen += desplazamiento
+                rotacionImagen += rotacion
+            }
+        }
+        .pointerInput(key1 = Unit){
+            detectTapGestures(
+                onDoubleTap = {
+                    //Devolvemos la imagen a su estado inicial
+                    escalaImagen = 1f
+                    rotacionImagen = 0f
+                    posicionImagen = Offset(x=0f,y=0f)
+                }
+            )
+        }, contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.turtle),
+            contentDescription = "Dibujo de tortuga",
+            modifier = Modifier.graphicsLayer(
+                scaleX = escalaImagen.coerceIn(0.5f, 5f),
+                scaleY = escalaImagen.coerceIn(0.5f, 5f),
+                translationX = posicionImagen.x,
+                translationY = posicionImagen.y,
+                rotationZ = rotacionImagen
+            )
+        )
+    }
+}
+
+
 @Preview
 @Composable
 fun DosTextosVerticalesPreview(){
-    Imagen()
+    ImagenZoom()
 }
 
 
