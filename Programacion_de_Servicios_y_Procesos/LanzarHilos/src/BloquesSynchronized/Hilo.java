874264ecb6b5a730cec72826911f6fde062eb595
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-10-09 14:29:46 +0200

    Más ejercicios con hilos

---
+++ Programacion_de_Servicios_y_Procesos/LanzarHilos/src/BloquesSynchronized/Hilo.java
@@ -0,0 +1,16 @@
+package BloquesSynchronized;
+
+public class Hilo implements Runnable{
+	Contadores cont = new Contadores();
+	@Override
+	public void run() {
+		for (int i = 0; i < 10000; i++) {
+			cont.incrementar1();
+		}
+		for (int i = 0; i < 10000; i++) {
+			cont.incrementar2();
+		}
+		
+	}
+
+}
---
+++ Programacion_de_Servicios_y_Procesos/LanzarHilos/src/BloquesSynchronized/Main.java
@@ -0,0 +1,22 @@
+package BloquesSynchronized;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Thread[] hilos = new Thread[10];
+		for (int i = 0; i < hilos.length; i++) {
+			hilos[i] = new Thread(new Hilo());
+			hilos[i].start();
+		for (int j = 0; j < 10; j++) {
+			try {
+				hilos[j].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		}
+
+	}
+
+}
