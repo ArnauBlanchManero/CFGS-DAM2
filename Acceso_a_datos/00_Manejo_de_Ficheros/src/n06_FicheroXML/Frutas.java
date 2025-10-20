e3c093e41a2697dd8a944f155044552767a406d3
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-10-20 11:29:24 +0200

    Frutas

--- Acceso_a_datos/00_Manejo_de_Ficheros/src/n06_FicheroXML/Frutas.java
+++ Acceso_a_datos/00_Manejo_de_Ficheros/src/n06_FicheroXML/Frutas.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,7 +17,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class Frutas {
-
+	static Scanner read = new Scanner(System.in);
 	public static void main(String[] args) {
 		File fichero = new File("frutas.xml");
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -27,7 +28,8 @@ public class Frutas {
 			NodeList lista = doc.getElementsByTagName("fruta");
 			int cantidad = lista.getLength();
 			List<String> nombres = new ArrayList<String>();
-			List<String> precios = new ArrayList<String>();
+			List<Double> precios = new ArrayList<Double>();
+			ArrayList<ArrayList<String>> matrizNutrinetes = new ArrayList<ArrayList<String>>();
 			for (int i = 0; i < cantidad; i++) {
 				Node nodo = lista.item(i);
 				if(nodo.getNodeType()==Node.ELEMENT_NODE) {
@@ -38,27 +40,80 @@ public class Frutas {
 					String color = fruta.getElementsByTagName("color").item(0).getTextContent();
 					String origen = fruta.getElementsByTagName("origen").item(0).getTextContent();
 					String precio = fruta.getElementsByTagName("precio").item(0).getTextContent();
-					precios.add(precio);
+					precios.add(Double.valueOf(precio));
 					String temporada = fruta.getElementsByTagName("temporada").item(0).getTextContent();
 					System.out.printf("La fruta %s es del tipo %s, de color %s, origen %s, con precio %s y temporada de %s.\n",nombre, tipo, color, origen, precio, temporada);
 					NodeList listaNutrientes = doc.getElementsByTagName("nutrientes");
 					Node nodoNutrientes = listaNutrientes.item(i);
 					Element nutrienteCant = (Element)nodoNutrientes;
 					int cantidadNutrientes = nutrienteCant.getElementsByTagName("nutriente").getLength();
+					ArrayList<String> listaNutrinete = new ArrayList<String>();
 					for (int j = 0; j < cantidadNutrientes; j++) {
 						if(nodoNutrientes.getNodeType()==Node.ELEMENT_NODE) {
 							Element nutriente = (Element)nodoNutrientes;
 							String nutrienteTxt = nutriente.getElementsByTagName("nutriente").item(j).getTextContent();
 							System.out.printf("\tNutriente %d: %s\n", j, nutrienteTxt);
+							listaNutrinete.add(nutrienteTxt);
 						}
 					}
+					matrizNutrinetes.add(listaNutrinete);
 				}
 			}
 			System.out.println("-----------------------");
 			System.out.println("Filtraciones por precio");
 			System.out.println("-----------------------");
-			String precioMax = precios.getFirst();
-			String precioMin = precios.getLast();
+			System.out.print("Por qué precio quieres filtrar: ");
+			double precioRef = read.nextDouble();
+			read.nextLine();
+			System.out.println("\nMayor: ");
+			int flag = 0;
+			for (int i = 0; i < nombres.size(); i++) {
+				if (precios.get(i)>precioRef) {
+					flag++;
+					System.out.println(nombres.get(i)+": "+precios.get(i)+"€");
+				}
+			}
+			if (flag==0) {
+				System.out.println("No se han encontrado precios mayores.");
+			}
+			flag = 0;
+			System.out.println("\nMenor: ");
+			for (int i = 0; i < nombres.size(); i++) {
+				if (precios.get(i)<precioRef) {
+					flag++;
+					System.out.println(nombres.get(i)+": "+precios.get(i)+"€");
+				}
+			}
+			if (flag==0) {
+				System.out.println("No se han encontrado precios menores.");
+			}
+			flag = 0;
+			System.out.println("\nIguales: ");
+			for (int i = 0; i < nombres.size(); i++) {
+				if (precios.get(i)==precioRef) {
+					flag++;
+					System.out.println(nombres.get(i)+": "+precios.get(i)+"€");
+				}
+			}
+			if (flag==0) {
+				System.out.println("No se han encontrado precios iguales.");
+			}
+			flag = 0;
+/*
+			int posicionMax = posicionPrecioMax(precios);
+			int posicionMin = posicionPrecioMin(precios);
+			Double precioMax = precios.get(posicionMax);
+			Double precioMin = precios.get(posicionMin);
+			System.out.println("La fruta con el precio más alto es " + nombres.get(posicionMax)+" con " + precioMax+"€");
+			System.out.println("La fruta con el precio más bajo es " + nombres.get(posicionMin)+" con " + precioMin+"€");
+*/
+			System.out.println("--------------------------");
+			System.out.println("Filtraciones por nutriente");
+			System.out.println("--------------------------");
+			System.out.print("Escribe el nutriente que quieres buscar: ");
+			String buscaNutriente = read.next();
+			read.nextLine();
+			
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -71,5 +126,29 @@ public class Frutas {
 		}
 		
 	}
+/*
+	private static int posicionPrecioMin(List<Double> precios) {
+		double precioMin = precios.get(0);
+		int posicion = 0;
+		for (int i = 1; i < precios.size(); i++) {
+			if (precios.get(i)<precioMin) {
+				precioMin = precios.get(i);
+				posicion = i;
+			}
+		}
+		return posicion;
+	}
 
+	private static int posicionPrecioMax(List<Double> precios) {
+		double precioMax = precios.get(0);
+		int posicion = 0;
+		for (int i = 1; i < precios.size(); i++) {
+			if (precios.get(i)>precioMax) {
+				precioMax = precios.get(i);
+				posicion = i;
+			}
+		}
+		return posicion;
+	}
+*/
 }
