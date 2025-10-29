bc882f36b5fc8211609e49bf7fc3edd5ce702ed4
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-10-29 21:36:42 +0100

    Acabo el ejercicio para que no tenga errores

--- Ciberseguridad/Integridad/src/Códigos_CRC/ValidarDNI.java
+++ Ciberseguridad/Integridad/src/Códigos_CRC/ValidarDNI.java
@@ -15,31 +15,39 @@ public class ValidarDNI {
 		}
 		// Calcular la letra
 		char letra = calcula_letra(nDNI);
-		System.out.println("Tu DNI es "+nDNI+letra);
+		if(letra!='0')
+			System.out.println("Tu DNI es "+nDNI+letra);
 	}
 	
-	private static char calcula_letra(String nDNI) {
+	private static char calcula_letra(String dni) {
 		char letra;
-		int resto =toInt(nDNI)%23;
+		long nDNI = toInt(dni);
+		if(nDNI==-1) 
+			return '0';
+		long resto =nDNI%23;
 		char[] letras_posibles = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-		letra = letras_posibles[resto];
+		letra = letras_posibles[(int)resto];
 		return letra;
 	}
 	
-	private static int toInt(String num) {
+	private static long toInt(String num) {
 		char[] num_separados = new char[8];
 		num_separados = num.toCharArray();
-		int num_largo=0;
-		for (int i = num_separados.length, j=0; i > 0; i--, j++) {
-			num_largo += num_separados[j]*10*i;
-			System.out.println(num_largo);
+		long num_largo=0;
+		for (int i = num_separados.length-1, j=0; i >= 0; i--, j++) {
+			num_largo += (num_separados[j]-48)*Math.pow(10, i);
+			if(num_separados[j]-48>9) {
+				System.out.println("Error.");
+				return -1;
+			}
+			//System.out.println(num_largo);
 		}
 		return num_largo;
 	}
 
 	private static boolean comprobar_num(String nDNI) {
 		int vueltas = nDNI.length();
-		System.out.println(vueltas);
+		//System.out.println(vueltas);
 		if (vueltas==8)
 			return true;
 		return false;
