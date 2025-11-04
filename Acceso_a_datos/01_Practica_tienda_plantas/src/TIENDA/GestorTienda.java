af4ed0b7cbc35419ff37e17d804f911f7e302225
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-11-04 13:37:30 +0100

    Implemento la gestion de los empleados

--- Acceso_a_datos/01_Practica_tienda_plantas/src/TIENDA/GestorTienda.java
+++ Acceso_a_datos/01_Practica_tienda_plantas/src/TIENDA/GestorTienda.java
@@ -1,12 +1,18 @@
 package TIENDA;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GestorTienda {
-
+	static Scanner read = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("BIENBENIDO AL GESTOR DE LA TIENDA DE PLANTAS");
 		System.out.println("============================================");
@@ -16,18 +22,117 @@ public class GestorTienda {
 		if(crear_jerarquía_ficheros()) {
 			plantas = cargar_datos_plantas();
 			empleados = cargar_datos_empleados();
-			if (empleados == null) {
-				System.out.println("No se hay empleados para trabajar.");
+			if (empleados != null) {
+				// iniciar sesión por empleado
+				int intentos = 3;
+				Empleado empleado = iniciar_sesión(empleados);
+				while (empleado == null && intentos > 0) {
+					System.out.println("El id o la contraseña están mal, te quedan "+intentos+ " intentos.");
+					empleado = iniciar_sesión(empleados);
+					intentos --;
+				}
+				if (intentos > 0) {
+					System.out.println("Bienvenido "+empleado.getNombre());
+					// TODO funciones menus empleado
+					if (empleado.getCargo().equals("Vendedor")) {
+						menu_vendedor(empleado, plantas);
+					}else if (empleado.getCargo().equals("Gestor")) {
+						menu_gestor(empleado, plantas, empleados);
+					} else {
+						System.out.println("No se ha encontrado el cargo del empleado.");
+					}
+					// TODO guardar datos
+					guardar_datos(plantas, empleados);
+				} else {
+					System.out.println("Te has quedado sin intentos, reinicia la aplicación.");
+				}
+			} else {
+				System.out.print("¿Quieres añadir los empleados por defecto? (si o no): ");
+				if(read.next().equalsIgnoreCase("si")) {
+					crear_empleados_por_defecto();
+				}
+				read.nextLine();
 			}
-			// TODO iniciar por empleado
-			// TODO funciones menus empleado
-			// TODO guardar datos
+		}
+	}
+
+	private static void guardar_datos(List<Planta> plantas, List<Empleado> empleados) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void menu_gestor(Empleado empleado, List<Planta> plantas, List<Empleado> empleados) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void menu_vendedor(Empleado empleado, List<Planta> plantas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static Empleado iniciar_sesión(List<Empleado> empleados) {
+		Empleado seleccionado = null;
+		System.out.println("\nIniciar sesión\n");
+		System.out.print("Id: ");
+		String id = read.nextLine();
+		System.out.print("Contraseña: ");
+		String passwd = read.nextLine();
+		for (int i = 0; i < empleados.size(); i++) {
+			if (id.compareToIgnoreCase(Integer.toString(empleados.get(i).getId()))==0) {
+				if (passwd.compareTo(empleados.get(i).getPasswd())==0) {
+					seleccionado = empleados.get(i);
+				}
+			}
+		}
+		return seleccionado;
+	}
+
+	private static void crear_empleados_por_defecto() {
+		File listaEmpleadosFichero = new File("EMPLEADOS/empleados.dat");
+		ArrayList<Empleado> listaEmpleados = new ArrayList<Empleado>();
+		try {
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(listaEmpleadosFichero.getPath()));
+		listaEmpleados.add(new Empleado(1452, "Teresa", "asb123", "Vendedor"));
+		listaEmpleados.add(new Empleado(156, "Miguel Ángel", "123qwes", "Gestor"));
+		listaEmpleados.add(new Empleado(7532, "Natalia", "xs21qw4", "Gestor"));
+		out.writeObject(listaEmpleados);
+		out.close();
+		System.out.println("Empleados añadidos.");
+		} catch (FileNotFoundException e) {
+			System.out.println("Ha ocurrido un error en la escritura del fichero empleados.dat.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Ha ocurrido un error en la escritura del fichero empleados.dat.");
+			e.printStackTrace();
 		}
 	}
 
 	private static List<Empleado> cargar_datos_empleados() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		File listaEmpleadosFichero = new File("EMPLEADOS/empleados.dat");
+		ArrayList<Empleado> listaEmpleados = new ArrayList<Empleado>();
+		ObjectInputStream ois;
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(listaEmpleadosFichero.getPath());
+			ois = new ObjectInputStream(fis);
+			listaEmpleados = (ArrayList<Empleado>) ois.readObject();
+			ois.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Ha ocurrido un error en la lectura del fichero empleados.dat.");
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			System.out.println("Ha ocurrido un error en la lectura del fichero empleados.dat.");
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			System.out.println("Ha ocurrido un error en la lectura del fichero empleados.dat.");
+			e.printStackTrace();
+			return null;
+		}
+		return listaEmpleados;
 	}
 
 	private static List<Planta> cargar_datos_plantas() {
--- Acceso_a_datos/01_Practica_tienda_plantas/src/TIENDA/Planta.java
+++ Acceso_a_datos/01_Practica_tienda_plantas/src/TIENDA/Planta.java
@@ -7,4 +7,55 @@ public class Planta {
 	String descripcion;
 	float precio;
 	int stock;
+	public Planta(int codigo, String nombre, String foto, String descripcion, float precio, int stock) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.foto = foto;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.stock = stock;
+	}
+	@Override
+	public String toString() {
+		return "Planta [codigo=" + codigo + ", nombre=" + nombre + ", foto=" + foto + ", descripcion=" + descripcion
+				+ ", precio=" + precio + ", stock=" + stock + "]";
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
 }
