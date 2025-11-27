6b2ff03cb606bc0245c30ed2d3b6fed7d3ae0629
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-11-27 12:07:42 +0100

    Enviar correos desde Java

---
+++ Desarrollo_de_interfaces/WindowBuilder/src/Email/SimpleEmail.java
@@ -0,0 +1,51 @@
+package Email;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class SimpleEmail {	
+	/**
+	   Outgoing Mail (SMTP) Server
+	   requires TLS or SSL: smtp.gmail.com (SSL)
+	   Use Authentication: Yes
+	   Port for SSL: 465
+	 */
+	public static void prepararEmail(String nombreEmail, String asunto, String cuerpo) {
+		
+		final String fromEmail = "arnau.blanch.dosa@gmail.com"; //EMAIL DE SALIDA
+		final String password = "****"; //CONTRASEÑA DEL EMAIL DE SALIDA (aplicaciones de 3ros) Contraseñas de aplicación -- Verificación en 2 pasos 
+		// https://myaccount.google.com/apppasswords
+		final String toEmail = nombreEmail; // EMAIL DESTINATARIO
+		
+		System.out.println("Configurando datos conexión SSL");
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP de GMAIL en este caso
+		props.put("mail.smtp.socketFactory.port", "465"); //PUERTO SSL 
+		props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+		props.put("mail.smtp.auth", "true"); //ACTIVAR SMTP AUTENTIFICACI�N
+		props.put("mail.smtp.port", "465"); //SMTP Port		
+		Authenticator auth = new Authenticator() {		
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};		
+		Session session = Session.getDefaultInstance(props, auth);//CREA UNA SESIÓN CON TODAS LAS PROPIEDADES Y EL "LOGIN"
+		System.out.println("Sesión Creada");
+		
+		/**
+		 * Llamada al método sendEmail con todos los datos configurados
+		 * session 
+		 * toEmail 
+		 * subject
+		 * body 
+		 */		
+	    EmailUtil.sendEmail(session, toEmail, asunto, cuerpo);
+	}
+}
+
