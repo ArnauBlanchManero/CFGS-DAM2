d19e092f56ee30078f82fa98486adc843c47ccc0
ArnauBlanchManero <arnau.b.manero@gmail.com>
2025-12-17 13:49:29 +0100

    Consultas neo4j

---
+++ Acceso_a_datos/05_Neo4j/src/main/java/n03_ActualizarNodo/Actualizar.java
@@ -0,0 +1,10 @@
+package n03_ActualizarNodo;
+
+public class Actualizar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Acceso_a_datos/05_Neo4j/src/main/java/n04_EliminarNodo/Eliminar.java
@@ -0,0 +1,10 @@
+package n04_EliminarNodo;
+
+public class Eliminar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
